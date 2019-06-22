142604b898526340d4e73de64a0d0a92f964f814
theschnaz <theschnaz@gmail.com>
2019-06-22 13:47:43 -0400

    first major push

---
+++ src/Components/About.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var poker = this.props.data.poker;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Gregory Schnese Profile Pic" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>Hello, I'm Gregory Schnese</h2>
+
+            <p>{bio}</p>
+            <p>Note to self, break up the status in the above copy into variables in the json and then JS.</p>
+            <p>{poker}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Email</h2>
+                  <p className="address">
+                     <span>{email}</span>
+					        </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;

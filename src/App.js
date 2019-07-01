7f3399253f409533bfab2c7c58b4b1a1258c9743
theschnaz <theschnaz@gmail.com>
2019-07-01 19:16:49 -0400

    small css, adding google analytics

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize('UA-143183510-1');
     ReactGA.pageview(window.location.pathname);
 
   }

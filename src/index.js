49f9d8270d973fa620e4f2dd9b858ef61086eed6
theschnaz <theschnaz@gmail.com>
2019-08-05 18:23:40 -0400

    trying react-snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM { hydrate, render } from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -15,5 +15,14 @@ const routing = (
   </Router>
 )
 
-ReactDOM.render(routing, document.getElementById('root'));
+//ReactDOM.render(routing, document.getElementById('root'));
+
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(routing, rootElement);
+} else {
+  render(routing, rootElement);
+}
 registerServiceWorker();
